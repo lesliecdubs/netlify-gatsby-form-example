7f09b66a6da334e6412b00bee363afb51916bd6a
lesliecdubs <leslie@netlify.com>
2019-04-05 17:30:48 -0500

    First commit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,55 +1,60 @@
 import React from 'react'
 
+// https://www.netlify.com/blog/2017/07/20/how-to-integrate-netlifys-form-handling-in-a-react-app/#form-handling-with-static-site-generators
+
 const Contact = (props) => (
-    <section id="contact">
-        <div className="inner">
-            <section>
-                <form method="post" action="#">
-                    <div className="field half first">
-                        <label htmlFor="name">Name</label>
-                        <input type="text" name="name" id="name" />
-                    </div>
-                    <div className="field half">
-                        <label htmlFor="email">Email</label>
-                        <input type="text" name="email" id="email" />
-                    </div>
-                    <div className="field">
-                        <label htmlFor="message">Message</label>
-                        <textarea name="message" id="message" rows="6"></textarea>
-                    </div>
-                    <ul className="actions">
-                        <li><input type="submit" value="Send Message" className="special" /></li>
-                        <li><input type="reset" value="Clear" /></li>
-                    </ul>
-                </form>
-            </section>
-            <section className="split">
-                <section>
-                    <div className="contact-method">
-                        <span className="icon alt fa-envelope"></span>
-                        <h3>Email</h3>
-                        <a href="#">information@untitled.tld</a>
-                    </div>
-                </section>
-                <section>
-                    <div className="contact-method">
-                        <span className="icon alt fa-phone"></span>
-                        <h3>Phone</h3>
-                        <span>(000) 000-0000 x12387</span>
-                    </div>
-                </section>
-                <section>
-                    <div className="contact-method">
-                        <span className="icon alt fa-home"></span>
-                        <h3>Address</h3>
-                        <span>1234 Somewhere Road #5432<br />
-                        Nashville, TN 00000<br />
-                        United States of America</span>
-                    </div>
-                </section>
-            </section>
-        </div>
-    </section>
+  <section id="contact">
+    <div className="inner">
+      <section>
+      {/* <form method="post" action="#"> */}
+      <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+      {/* You still need to add the hidden input with the form name to your JSX form */}
+        <input type="hidden" name="form-name" value="contact" />
+          <div className="field half first">
+            <label htmlFor="name">Name</label>
+            <input type="text" name="name" id="name" />
+          </div>
+          <div className="field half">
+            <label htmlFor="email">Email</label>
+            <input type="text" name="email" id="email" />
+          </div>
+          <div className="field">
+            <label htmlFor="message">Message</label>
+            <textarea name="message" id="message" rows="6"></textarea>
+          </div>
+          <ul className="actions">
+            <li><input type="submit" value="Send Message" className="special" /></li>
+            <li><input type="reset" value="Clear" /></li>
+          </ul>
+        </form>
+      </section>
+      <section className="split">
+        <section>
+          <div className="contact-method">
+            <span className="icon alt fa-envelope"></span>
+            <h3>Email</h3>
+            <a href="#">information@untitled.tld</a>
+          </div>
+        </section>
+        <section>
+          <div className="contact-method">
+            <span className="icon alt fa-phone"></span>
+            <h3>Phone</h3>
+            <span>(000) 000-0000 x12387</span>
+          </div>
+        </section>
+        <section>
+          <div className="contact-method">
+            <span className="icon alt fa-home"></span>
+            <h3>Address</h3>
+            <span>1234 Somewhere Road #5432<br />
+            Nashville, TN 00000<br />
+            United States of America</span>
+          </div>
+        </section>
+      </section>
+    </div>
+  </section>
 )
 
 export default Contact
